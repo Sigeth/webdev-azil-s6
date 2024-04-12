efac95c0cd5db8855138b5d0ec751c3c6cf7eb86
Sigeth <52868615+Sigeth@users.noreply.github.com>
2024-04-12 13:25:05 +0200

    feat: added LocationDetailComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LocationDetailComponent } from './location-detail/location-detail.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'location-detail', component: LocationDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationDetailComponent } from './location-detail/location-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationDetailComponent
   ],
   imports: [
     BrowserModule,
