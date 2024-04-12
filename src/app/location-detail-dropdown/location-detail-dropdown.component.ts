709d70cde2a118850315db57cd9f65f5b5169d8f
Maxou <Jackson.Kyoshi@gmail.com>
2024-04-12 20:40:40 +0200

    feat: dropdown menu working

--- src/app/location-detail-dropdown/location-detail-dropdown.component.ts
+++ src/app/location-detail-dropdown/location-detail-dropdown.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-location-detail-dropdown',
   templateUrl: './location-detail-dropdown.component.html',
-  styleUrl: './location-detail-dropdown.component.css'
+  styleUrl: './location-detail-dropdown.component.css',
 })
 export class LocationDetailDropdownComponent {
+
+  @Input() menu?: number;
+  
+  form: boolean = false;
+
+  afficherSecondComposant() {
+    if(this.form == false){
+      this.form = true;
+      console.log(this.menu)
+    }
+    else{
+      this.form = false;
+    }
+  }
 
 }
