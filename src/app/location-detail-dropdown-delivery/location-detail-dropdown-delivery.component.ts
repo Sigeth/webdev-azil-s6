401c9c75dc78475b16ec2b13dd9ae2ceb3ceb6a6
Sigeth <52868615+Sigeth@users.noreply.github.com>
2024-05-24 16:49:09 +0200

    feat: better delivery informations

--- src/app/location-detail-dropdown-delivery/location-detail-dropdown-delivery.component.ts
+++ src/app/location-detail-dropdown-delivery/location-detail-dropdown-delivery.component.ts
@@ -28,4 +28,66 @@ export class LocationDetailDropdownDeliveryComponent {
     }
   ];
   // deliveryInformations: DeliveryInformation[] = [];
+
+  sortTableDevice() {
+    this.deliveryInformations.sort((el1: DeliveryInformation, el2: DeliveryInformation) => {
+      if (el1.device > el2.device) {
+        return 1;
+      }
+
+      if (el1.device < el2.device) {
+        return -1;
+      }
+
+      return 0;
+    })
+  }
+
+  sortTableModel() {
+    this.deliveryInformations.sort((el1: DeliveryInformation, el2: DeliveryInformation) => {
+      if (el1.model > el2.model) {
+        return 1;
+      }
+
+      if (el1.model < el2.model) {
+        return -1;
+      }
+
+      return 0;
+    })
+  }
+
+  sortTableNumber() {
+    this.deliveryInformations.sort((el1: DeliveryInformation, el2: DeliveryInformation) => {
+      if (el1.number > el2.number) {
+        return 1;
+      }
+
+      if (el1.number < el2.number) {
+        return -1;
+      }
+
+      return 0;
+    })
+  }
+
+  sortTableDate() {
+    this.deliveryInformations.sort((el1: DeliveryInformation, el2: DeliveryInformation) => {
+      let date1 = new Date(el1.expected_delivery);
+      let date2 = new Date(el2.expected_delivery);
+      console.log(date1);
+      console.log(date2);
+
+      //TODO: change expected_delivery to Date
+      if (date1 > date2) {
+        return 1;
+      }
+
+      if (date1 < date2) {
+        return -1;
+      }
+
+      return 0;
+    })
+  }
 }
