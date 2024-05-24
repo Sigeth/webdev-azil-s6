07619db959d22c8cc0f64af84a4b6ac8b4e6cfe0
Sigeth <52868615+Sigeth@users.noreply.github.com>
2024-05-24 13:42:54 +0200

    feat: buttons in administrative dropdown works

--- src/app/location-detail-dropdown-administrative/location-detail-dropdown-administrative.component.ts
+++ src/app/location-detail-dropdown-administrative/location-detail-dropdown-administrative.component.ts
@@ -10,22 +10,42 @@ export class LocationDetailDropdownAdministrativeComponent {
 
   constructor() {
     for (let i=0; i<3; i++) {
-      this.contactAvailabilities[i] = [true, true, true, false, true, true, true]
+      this.contactAvailabilities[i] = [true, true, true, true, true, false, false]
     }
   }
 
   checkHandler(i: number, j: number) {
     let checkboxChecked = this.contactAvailabilities[i][j]
     if (i == 0) {
-      if (checkboxChecked) {
+      if (!checkboxChecked) {
         this.contactAvailabilities[1][j] = true;
         this.contactAvailabilities[2][j] = true;
       }
     } else {
       if(i == 1) {
-        this.contactAvailabilities[0][j] = this.contactAvailabilities[2][j] && checkboxChecked;
+        this.contactAvailabilities[0][j] = this.contactAvailabilities[2][j] && !checkboxChecked;
       } else {
-        this.contactAvailabilities[0][j] = this.contactAvailabilities[1][j] && checkboxChecked;
+        this.contactAvailabilities[0][j] = this.contactAvailabilities[1][j] && !checkboxChecked;
+      }
+    }
+    this.contactAvailabilities[i][j] = !checkboxChecked
+  }
+
+  check_24h_7() {
+    for (let i=0; i<3; i++) {
+      for (let j = 0; j<7; j++) {
+        this.contactAvailabilities[i][j] = true;
+      }
+    }
+  }
+
+  check_weekdays() {
+    for (let i=0; i<3; i++) {
+      for (let j = 0; j<5; j++) {
+        this.contactAvailabilities[i][j] = true;
+      }
+      for (let j=5; j<7; j++) {
+        this.contactAvailabilities[i][j] = false;
       }
     }
   }
